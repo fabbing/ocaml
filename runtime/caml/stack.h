0244d9b396d144405a9759d37e20bc08741a2fe2
fabbing <fabrice@tarides.com>
2022-03-03 16:31:31 +0100

    Slightly improve Pop_frame_pointer macro for arm64

--- runtime/caml/stack.h
+++ runtime/caml/stack.h
@@ -63,7 +63,7 @@
    See arm64.S and arm64/proc.ml for the indices */
 #define Wosize_gc_regs (2 + 24 /* int regs */ + 24 /* float regs */)
 #define Saved_return_address(sp) *((intnat *)((sp) - 8))
-#define Pop_frame_pointer(sp) sp += sizeof(value)
+#define Pop_frame_pointer(sp) (sp) += sizeof(value)
 #define Context_needs_padding /* keep stack 16-byte aligned */
 #endif
 
